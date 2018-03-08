ff4395a9f7247e4fe659070cdbc4389a3faab25d
victor <victor@victor-PC.home>
2018-03-08 22:00:43 +0100

    edition en cours

---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gestion de la Mobilité des Etudiants</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="<?= base_url('assets')?>/open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+        <link href="<?= base_url('assets/css')?>/main.css" rel="stylesheet">
+    </head>
+    <body>
